750bb3bb9f46d097fb3e7a72a9b2a7a423affa2d
Chris Cooley <ccooley@bonadmin.com>
2024-07-16 12:45:17 -0600

    Add ability to type into in form

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
+    const [name, setName] = useState("");
+
     return (
-        <></>
+        <>
+            <form>
+                <div>
+                    <label htmlFor="...">Name</label>
+                    <input 
+                        type="text"
+                        id="name"
+                        value={name}
+                        placeholder="Enter name here"
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+                            setName(event.target.value)}/>
+                </div>
+            </form>
+        </>
     );
 }
 
